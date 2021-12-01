e3b3129321b05893572087d090fc7c219fdb3e86
ASleepingBazza <elias.e.fredriksson@gmail.com>
2021-12-01 13:14:24 +0100

    Fixed so dist is in gitignore

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,29 @@
-window.onload = function() {
-    
-}
+import {
+    Product,
+    createProductObjectsFromData,
+    getFilterProducts,
+    getCategoryProducts,
+    getProductById,
+} from "./models/produkt";
+
+window.onload = function () {
+    //TESTS
+    //get all products
+    let products: Product[] = createProductObjectsFromData();
+    console.log("ALL: ", products);
+
+    //get filtered products
+    let filteredProducts: Product[] = getFilterProducts(
+        "Jättekonstig",
+        products
+    );
+    console.log("FILTERED: ", filteredProducts);
+
+    //get categorized products
+    let categorizedProducts: Product[] = getCategoryProducts("skor", products);
+    console.log("CATEGORIZED: ", categorizedProducts);
+
+    //get product by id
+    let product: Product = getProductById(3, products);
+    console.log("BY ID: ", product);
+};
