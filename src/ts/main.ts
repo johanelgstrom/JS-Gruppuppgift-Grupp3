9f94e892ee21924ca821929a9769ad79a9027cfe
Johan Elgström <johan.elgstrom@gmail.com>
2021-12-05 15:10:22 +0100

    burger position fix, menu items added

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -10,8 +10,10 @@ import { Sort, sortProductsBy } from "./models/sorting";
 
 window.onload = function (): void {
     runTests();
-    let mobileBurger: HTMLDivElement = document.getElementById("burger-menu") as HTMLDivElement;
+    let mobileBurger: HTMLDivElement = document.getElementById("burger-menu-phone") as HTMLDivElement;
     mobileBurger.addEventListener("click", menuSlideIn);
+    let burger: HTMLDivElement = document.getElementById("burger-menu") as HTMLDivElement;
+    burger.addEventListener("click", menuSlideIn);
 };
 
 function runTests(): void {
@@ -46,18 +48,27 @@ function runTests(): void {
 
 function menuSlideIn() {
     let menu: HTMLDivElement = document.getElementById("menu-container") as HTMLDivElement;
+    let topBunPhone: HTMLDivElement = document.getElementById("top-bun-phone") as HTMLDivElement;
+    let pattyPhone: HTMLDivElement = document.getElementById("patty-phone") as HTMLDivElement;
+    let bottomBunPhone: HTMLDivElement = document.getElementById("bottom-bun-phone") as HTMLDivElement;
     let topBun: HTMLDivElement = document.getElementById("top-bun") as HTMLDivElement;
     let patty: HTMLDivElement = document.getElementById("patty") as HTMLDivElement;
     let bottomBun: HTMLDivElement = document.getElementById("bottom-bun") as HTMLDivElement;
 
     if (menu.classList.contains("show-menu")) {
         menu.classList.remove("show-menu");
+        topBunPhone.classList.remove("top-bun-toggle");
+        pattyPhone.classList.remove("patty-toggle");
+        bottomBunPhone.classList.remove("bottom-bun-toggle");
         topBun.classList.remove("top-bun-toggle");
         patty.classList.remove("patty-toggle");
         bottomBun.classList.remove("bottom-bun-toggle");
     } 
     else {
         menu.classList.add("show-menu");
+        topBunPhone.classList.add("top-bun-toggle");
+        pattyPhone.classList.add("patty-toggle");
+        bottomBunPhone.classList.add("bottom-bun-toggle");
         topBun.classList.add("top-bun-toggle");
         patty.classList.add("patty-toggle");
         bottomBun.classList.add("bottom-bun-toggle");
