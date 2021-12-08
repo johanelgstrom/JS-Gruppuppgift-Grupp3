fa6c4d36089ebed902b7e44ca8a1e1295c1e550f
Filipengbergs <filip792@hotmail.com>
2021-12-08 15:21:10 +0100

    Fixed image/title/price element

--- src/ts/browse.ts
+++ src/ts/browse.ts
@@ -44,24 +44,40 @@ function createElementForProducts() {
             "img"
         ) as HTMLImageElement;
         productImage.src = product.imageUrl[0];
+        productImage.alt = "Image";
 
         // PRODUCT TITLE //
+        let titleContainer: HTMLDivElement = document.createElement(
+            "div"
+        ) as HTMLDivElement;
         let productName: HTMLSpanElement = document.createElement(
             "span"
         ) as HTMLSpanElement;
+        titleContainer.className = "title-container";
         productName.innerHTML = product.name;
 
         // PRODUCT PRICE //
+        let priceContainer: HTMLDivElement = document.createElement(
+            "div"
+        ) as HTMLDivElement;
+        let currencyText: HTMLSpanElement = document.createElement(
+            "span"
+        ) as HTMLSpanElement;
         let productPrice: HTMLSpanElement = document.createElement(
             "span"
         ) as HTMLSpanElement;
+        priceContainer.className = "price-container";
+        currencyText.innerHTML = "kr";
         productPrice.innerHTML = product.price.toString();
 
         // APPENDS //
-        productContainer.appendChild(productItem);
-        productItem.appendChild(imageContainer);
         imageContainer.appendChild(productImage);
-        productItem.appendChild(productName);
-        productItem.appendChild(productPrice);
+        productItem.appendChild(imageContainer);
+        titleContainer.appendChild(productName);
+        productItem.appendChild(titleContainer);
+        priceContainer.appendChild(productPrice);
+        priceContainer.appendChild(currencyText);
+        productItem.appendChild(priceContainer);
+        productContainer.appendChild(productItem);
     }
 }
